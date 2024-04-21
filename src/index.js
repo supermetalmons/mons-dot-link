498add4439c0af8f6b0cdc82675887dfda8f49d3
ivan grachev <ivan@ivan.lol>
2024-04-21 22:08:04 +0300

    put mons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import init, { hello } from 'mons-web';
+import init, { hello } from "mons-web";
 
 async function run() {
   await init();
@@ -25,11 +25,73 @@ for (let y = 0; y < 11; y++) {
   }
 }
 
-const drainerImage = document.createElementNS("http://www.w3.org/2000/svg", "image");
-drainerImage.setAttributeNS("http://www.w3.org/1999/xlink", "href", "../img/drainer.png");
-drainerImage.setAttribute("width", 1);
-drainerImage.setAttribute("height", 1);
-drainerImage.setAttribute("class", "item");
+const drainer = loadImage("drainer");
+const angel = loadImage("angel");
+const demon = loadImage("demon");
+const spirit = loadImage("spirit");
+const mystic = loadImage("mystic");
+const mana = loadImage("mana");
+const drainerB = loadImage("drainer-black");
+const angelB = loadImage("angel-black");
+const demonB = loadImage("demon-black");
+const spiritB = loadImage("spirit-black");
+const mysticB = loadImage("mystic-black");
+const manaB = loadImage("mana-black");
+const bombOrPotion = loadImage("bombOrPotion");
+const bomb = loadImage("bomb");
+const potion = loadImage("potion");
+const supermana = loadImage("supermana");
+const supermanaSimple = loadImage("supermana-simple");
+
+placeItem(demon, 3, 10);
+placeItem(angel, 4, 10);
+placeItem(drainer, 5, 10);
+placeItem(spirit, 6, 10);
+placeItem(mystic, 7, 10);
+
+placeItem(demonB, 7, 0);
+placeItem(angelB, 6, 0);
+placeItem(drainerB, 5, 0);
+placeItem(spiritB, 4, 0);
+placeItem(mysticB, 3, 0);
+
+placeItem(manaB, 4, 3);
+placeItem(manaB, 6, 3);
+placeItem(manaB, 3, 4);
+placeItem(manaB, 5, 4);
+placeItem(manaB, 7, 4);
+
+placeItem(mana, 3, 6);
+placeItem(mana, 4, 7);
+placeItem(mana, 5, 6);
+placeItem(mana, 6, 7);
+placeItem(mana, 7, 6);
+
+placeItem(bombOrPotion, 0, 5);
+placeItem(bombOrPotion, 10, 5);
+placeItem(supermana, 5, 5);
+
+function loadImage(name) {
+  const image = document.createElementNS("http://www.w3.org/2000/svg", "image");
+  image.setAttributeNS(
+    "http://www.w3.org/1999/xlink",
+    "href",
+    `../img/${name}.png`
+  );
+  image.setAttribute("width", 1);
+  image.setAttribute("height", 1);
+  image.setAttribute("class", "item");
+  return image;
+}
+
+function placeItem(item, x, y) {
+  const img = item.cloneNode();
+  img.setAttribute("x", x);
+  img.setAttribute("y", y);
+  overlay.appendChild(img);
+  const key = `item-${x}-${y}`;
+  images[key] = img;
+}
 
 function toggleItem(x, y) {
   const key = `item-${x}-${y}`;
@@ -37,26 +99,6 @@ function toggleItem(x, y) {
     images[key].remove();
     delete images[key];
   } else {
-    const img = drainerImage.cloneNode();
-    img.setAttribute("x", x);
-    img.setAttribute("y", y);
-    overlay.appendChild(img);
-    images[key] = img;
+    placeItem(drainer, x, y);
   }
-}
-
-placeRandomMons();
-
-function placeRandomMons() {
-  const positions = new Set();
-  while (positions.size < 9) {
-    const x = Math.floor(Math.random() * 11);
-    const y = Math.floor(Math.random() * 11);
-    positions.add(`${x}-${y}`);
-  }
-
-  positions.forEach((pos) => {
-    const [x, y] = pos.split("-").map(Number);
-    toggleItem(x, y);
-  });
 }
