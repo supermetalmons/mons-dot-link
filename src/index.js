a6f8ce8087c2658929b69866dac6a51e86c5dd13
ivan grachev <ivan@ivan.lol>
2024-04-21 20:05:39 +0300

    remove unused packages

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ async function run() {
 
 run();
 
-const bytes = Buffer.from('hello', 'utf8');
-const encoded = bs58.encode(bytes);
-console.log(encoded);
-const decoded = bs58.decode(encoded);
-console.log(decoded.toString('utf8'));
-
 const drainerImage = document.getElementById("preload-drainer-image").src;
 const overlay = document.getElementById("overlay");
 const images = {};
