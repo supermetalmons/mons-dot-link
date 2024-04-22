80d279486c3d275c0c60916b06aa696382839871
ivan grachev <ivan@ivan.lol>
2024-04-22 20:36:48 +0300

    use all mons game model apis

--- src/index.js
+++ src/index.js
@@ -7,18 +7,25 @@ async function run() {
   const modifier = Modifier.SelectPotion;
   const location = new Location(10, 10);
   const fen = "0 0 w 0 0 0 0 0 1 n03y0xs0xd0xa0xe0xn03/n11/n11/n04xxmn01xxmn04/n03xxmn01xxmn01xxmn03/xxQn04xxUn04xxQ/n03xxMn01xxMn01xxMn03/n04xxMn01xxMn04/n11/n11/n03E0xA0xD0xS0xY0xn03";
+
   const game = MonsGameModel.from_fen(fen);
   const moves = game.available_move_kinds();
+  const output = game.process_input([], undefined);
+  const outputFromFenInput = game.process_input_fen("");
+  const squareAt = game.square(new Location(5, 5));
+  const itemAt = game.item(location);
+  const isLaterThan = game.is_later_than(fen);
+  const activeColor = game.active_color();
+  const winnerColor = game.winner_color();
+  const blackScore = game.black_score();
+  const whiteScore = game.white_score();
+  const locationsWithContent = game.locations_with_content();
 
   if (fen != game.fen()) {
     throw new Error("smth is wrong with a fen");
   }
 
-  console.log(moves);
-  console.log(game.fen());
-  console.log(color);
-  console.log(modifier);
-  console.log(location.i, location.j);
+  console.log(color, modifier, moves, output, outputFromFenInput, squareAt, itemAt, isLaterThan, activeColor, winnerColor, blackScore, whiteScore, locationsWithContent);
 }
 
 run();
