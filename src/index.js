1d9e462e71453f90c8f4f00c7b17c33d8c7c0e6f
ivan grachev <ivan@ivan.lol>
2024-04-22 16:20:49 +0300

    use Location from wasm

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import init, { hello } from "mons-web";
+import init, { Location, Modifier, Color } from "mons-web";
 import { setupBoard } from "./board.js";
 
 async function run() {
   await init();
-  console.log(hello());
+  const color = Color.Black;
+  const modifier = Modifier.SelectPotion;
+  const location = new Location(10, 10);
+  console.log(color);
+  console.log(modifier);
+  console.log(location.i, location.j);
 }
 
 run();
