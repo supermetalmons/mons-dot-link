2ff9fd5865b186b4b3d0bd8eca2c532afc611697
ivan grachev <ivan@ivan.lol>
2024-04-23 15:36:14 +0300

    validate wasm new game

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ const location = new Location(0, 5);
 const fen =
   "0 0 w 0 0 0 0 0 1 n03y0xs0xd0xa0xe0xn03/n11/n11/n04xxmn01xxmn04/n03xxmn01xxmn01xxmn03/xxQn04xxUn04xxQ/n03xxMn01xxMn01xxMn03/n04xxMn01xxMn04/n11/n11/n03E0xA0xD0xS0xY0xn03";
 
-const game = MonsGameModel.from_fen(fen);
+const game = MonsGameModel.new();
+const gameFromFen = MonsGameModel.from_fen(fen);
 const moves = game.available_move_kinds();
 const output = game.process_input([], undefined);
 const outputFromFenInput = game.process_input_fen("");
@@ -28,7 +29,7 @@ const blackScore = game.black_score();
 const whiteScore = game.white_score();
 const locationsWithContent = game.locations_with_content();
 
-if (fen != game.fen()) {
+if (fen != game.fen() || fen != gameFromFen.fen()) {
   throw new Error("smth is wrong with a fen");
 }
 
