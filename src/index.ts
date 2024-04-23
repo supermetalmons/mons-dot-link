bdc888f8ecfef9bf03f5961c3cdc989423f8e5a4
ivan grachev <ivan@ivan.lol>
2024-04-23 19:08:31 +0300

    tune wasm imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import init, { MonsGameModel, Location, Modifier, Color, OutputModelKind } from "mons-web";
+import init, { MonsGameModel, Location as LocationModel, Modifier as ModifierModel, Color as ColorModel, OutputModelKind } from "mons-web";
 import { setupBoard, blinkLocations } from "./board";
 
 setupBoard();
@@ -10,9 +10,9 @@ export function didClickSquare(i: number, j: number) {
 
 await init();
 
-const color = Color.Black;
-const modifier = Modifier.SelectPotion;
-const location = new Location(0, 5);
+const color = ColorModel.Black;
+const modifier = ModifierModel.SelectPotion;
+const location = new LocationModel(0, 5);
 const fen =
   "0 0 w 0 0 0 0 0 1 n03y0xs0xd0xa0xe0xn03/n11/n11/n04xxmn01xxmn04/n03xxmn01xxmn01xxmn03/xxQn04xxUn04xxQ/n03xxMn01xxMn01xxMn03/n04xxMn01xxMn04/n11/n11/n03E0xA0xD0xS0xY0xn03";
 
@@ -21,7 +21,7 @@ const gameFromFen = MonsGameModel.from_fen(fen);
 const moves = game.available_move_kinds();
 const output = game.process_input([], undefined);
 const outputFromFenInput = game.process_input_fen("");
-const squareAt = game.square(new Location(5, 5));
+const squareAt = game.square(new LocationModel(5, 5));
 const itemAt = game.item(location);
 const isLaterThan = game.is_later_than(fen);
 const activeColor = game.active_color();
@@ -51,7 +51,7 @@ console.log("locations with content:", locationsWithContent);
 var currentInputs: {i: number; j: number;}[] = [];
 
 function processCurrentInputs() {
-  const gameInput = currentInputs.map(input => new Location(input.i, input.j));
+  const gameInput = currentInputs.map(input => new LocationModel(input.i, input.j));
   let output = game.process_input(gameInput);
 
   switch (output.kind) {
