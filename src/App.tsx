216b61074a90c5eb441643cd2342c937f44c1429
ivan grachev <ivan@ivan.lol>
2024-09-25 18:49:47 +0300

    add rock logo menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import BoardComponent from "./BoardComponent";
 import { logoBase64 } from "./helpers/logo";
 
 const App: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="app-container">
       <div className="connect-button-container">
@@ -18,20 +24,29 @@ const App: React.FC = () => {
       </div>
       <BoardComponent />
 
-      <a
-        className="rock-link"
-        href="https://opensea.io/collection/super-metal-mons-gen-2"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          position: "absolute",
-          top: "9pt",
-          left: "9pt",
-          display: "",
-        }}>
-        <img src={logoBase64}
-        style={{ width: "1.8rem", height: "1.8rem", opacity: 1 }} alt="Rock" />
-      </a>
+      <div className="rock-button-container">
+        <div
+          className="rock-button"
+          onClick={(e) => {
+            e.preventDefault();
+            toggleMenu();
+          }}
+          onMouseEnter={() => setIsMenuOpen(true)}
+          // onMouseLeave={() => setIsMenuOpen(false)} // TODO: auto-close when cursor is far away
+        >
+          <img src={logoBase64} alt="Rock" />
+        </div>
+        {isMenuOpen && (
+          <div className="rock-menu">
+            <button className="new-game-button">new game</button>
+            <a href="https://github.com/supermetalmons" target="_blank" rel="noopener noreferrer">github</a>
+            <a href="https://apps.apple.com/app/id6446702971" target="_blank" rel="noopener noreferrer">app store</a>
+            <a href="https://x.com/supermetalx" target="_blank" rel="noopener noreferrer">x</a>
+            <a href="https://opensea.io/collection/supermetalmons" target="_blank" rel="noopener noreferrer">mons gen1</a>
+            <a href="https://opensea.io/collection/super-metal-mons-gen-2" target="_blank" rel="noopener noreferrer">mons gen2</a>
+          </div>
+        )}
+      </div>
 
       <select
         className="voice-reaction-select"
