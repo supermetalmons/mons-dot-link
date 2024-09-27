61c653b5fef7af877ff5ea5167c8d57c1560f773
ivan grachev <ivan@ivan.lol>
2024-09-27 15:52:11 +0300

    clean up wagmi config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,22 +1,12 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
   base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'mons',
   projectId: 'YOUR_PROJECT_ID', // TODO: setup
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
     base,
-    ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
 });
