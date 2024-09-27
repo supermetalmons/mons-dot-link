0a298165d316a59911a19974b26f35d8d35ccec8
ivan grachev <ivan@ivan.lol>
2024-09-27 16:21:06 +0300

    fix wagmi config

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,12 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
   base,
+  mainnet,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'mons',
   projectId: 'YOUR_PROJECT_ID', // TODO: setup
   chains: [
+    mainnet,
     base,
   ],
 });
