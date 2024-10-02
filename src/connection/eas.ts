fce186a46a730e4c3f9128ace87e2856b6b6702e
ivan grachev <ivan@ivan.lol>
2024-10-02 17:03:05 +0300

    make multi attest by delegation proxy work

--- src/connection/eas.ts
+++ src/connection/eas.ts
@@ -1,4 +1,4 @@
-import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { EAS, SchemaEncoder, EIP712Proxy } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 
 async function getSigner(): Promise<any> {
@@ -8,8 +8,8 @@ async function getSigner(): Promise<any> {
 }
 
 export async function sendEasTx(txData) {
-    alert("soon");
-    // TODO: make it work
+  // sendEasTxWip(txData);
+  alert("soon");
 }
 
 export async function sendEasTxWip(txData) {
@@ -25,8 +25,12 @@ export async function sendEasTxWip(txData) {
 
   const signatures = txData.signatures;
   const attester = txData.attester;
-  const eas = new EAS("0x4200000000000000000000000000000000000021");
-  eas.connect(signer);
+
+  const newProxy = new EIP712Proxy("0xF095fE4b23958b08D38e52d5d5674bBF0C03cbF6", { signer: signer });
+  const eas = new EAS("0x4200000000000000000000000000000000000021", {
+    proxy: newProxy,
+    signer: signer,
+  });
 
   const schemaEncoder = new SchemaEncoder("uint64 gameId, uint64 points, bool isWin");
 
@@ -42,45 +46,7 @@ export async function sendEasTxWip(txData) {
     { name: "isWin", value: false, type: "bool" },
   ]);
 
-  const multiTxOk1 = await eas.multiAttestByDelegation([
-    {
-      schema: "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
-      data: [
-        {
-          recipient: "0xE26067c76fdbe877F48b0a8400cf5Db8B47aF0fE",
-          expirationTime: 0n,
-          revocable: false,
-          refUID: "0x0000000000000000000000000000000000000000000000000000000000000000",
-          data: encodedData1,
-          value: 0n,
-        },
-      ],
-      signatures: [signatures[0]],
-      attester: attester,
-      deadline: 0n,
-    },
-  ]);
-
-  const multiTxOk2 = await eas.multiAttestByDelegation([
-    {
-      schema: "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
-      data: [
-        {
-          recipient: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165",
-          expirationTime: 0n,
-          revocable: false,
-          refUID: "0x0000000000000000000000000000000000000000000000000000000000000000",
-          data: encodedData2,
-          value: 0n,
-        },
-      ],
-      signatures: [signatures[1]],
-      attester: attester,
-      deadline: 0n,
-    },
-  ]);
-
-  const multiTxAll = await eas.multiAttestByDelegation([
+  const multiTxAll = await eas.multiAttestByDelegationProxy([
     {
       schema: "0xb6cdeca57cf4618b9e6f619771b9ca43febd99de294a8de229aa4938405f2efa",
       data: [
@@ -107,11 +73,7 @@ export async function sendEasTxWip(txData) {
     },
   ]);
 
-  const newAttestationUID = await multiTxOk1.wait(); // OK
-  // const newAttestationUID = await multiTxOk2.wait(); // OK
-  // const newAttestationUID = await multiTxAll.wait(); // missing revert data (action="estimateGas"
-
-  // TODO: make it work with all attestaions in a single tx
-
+  const newAttestationUID = await multiTxAll.wait();
   console.log(newAttestationUID);
+  // TODO: communicate to a player
 }
