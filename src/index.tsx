f3f70ec13f995efc46945eae41f8478c61583efc
ivan grachev <ivan@ivan.lol>
2024-10-20 20:48:07 +0300

    stub background iframe

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ import { ConnectButton, RainbowKitAuthenticationProvider, RainbowKitProvider, li
 import BoardComponent from "./game/BoardComponent";
 import MainMenu from "./ui/MainMenu";
 import { config } from "./utils/wagmi";
-import { useAuthStatus, createAuthAdapter } from './connection/authentication';
-import { signIn } from './connection/connection';
+import { useAuthStatus, createAuthAdapter } from "./connection/authentication";
+import { signIn } from "./connection/connection";
 import BottomControls from "./ui/BottomControls";
 import { useBottomControlsActions } from "./ui/BottomControlsActions";
 
@@ -42,19 +42,26 @@ const App = () => {
               darkMode: darkTheme(),
             }}>
             <div className="app-container">
-              <div className="connect-button-container" onClick={handleConnectClick}>
-                <ConnectButton
-                  showBalance={false}
-                  chainStatus="none"
-                  accountStatus={{
-                    smallScreen: "avatar",
-                    largeScreen: "full",
-                  }}
-                />
+              {/* <iframe
+                className="background-iframe"
+                src="https://live.artblocks.io/random/0x68c01cb4733a82a58d5e7bb31bddbff26a3a35d5/18?useCustomViewParams=true&showText=false&backgroundColor=000000&width=100vw&height=100vh"
+                title="background"
+              ></iframe> */}
+              <div className="content-container">
+                <div className="connect-button-container" onClick={handleConnectClick}>
+                  <ConnectButton
+                    showBalance={false}
+                    chainStatus="none"
+                    accountStatus={{
+                      smallScreen: "avatar",
+                      largeScreen: "full",
+                    }}
+                  />
+                </div>
+                <BoardComponent />
+                <MainMenu />
+                <BottomControls actions={bottomControlsActions} />
               </div>
-              <BoardComponent />
-              <MainMenu />
-              <BottomControls actions={bottomControlsActions} />
             </div>
           </RainbowKitProvider>
         </RainbowKitAuthenticationProvider>
