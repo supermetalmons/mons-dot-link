c0d15ae0141471a53f756d7789229a4f149442a2
ivan grachev <ivan@ivan.lol>
2024-10-28 19:00:34 +0300

    stub onchain ratings menu item

--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -253,13 +253,18 @@ const MainMenu: React.FC = () => {
           </IconRow>
           <LinkRow>
             <LinkButton href="https://opensea.io/collection/supermetalmons" target="_blank" rel="noopener noreferrer">
-              gen 1
+              Gen 1
             </LinkButton>
             <LinkButton href="https://opensea.io/collection/super-metal-mons-gen-2" target="_blank" rel="noopener noreferrer">
-              gen 2
+              Gen 2
             </LinkButton>
             <LinkButton href="https://supermetalmons.com" target="_blank" rel="noopener noreferrer">
-              irl
+              IRL
+            </LinkButton>
+          </LinkRow>
+          <LinkRow>
+            <LinkButton href="https://base.easscan.org/schema/view/0x5c6e798cbb817442fa075e01b65d5d65d3ac35c2b05c1306e8771a1c8a3adb32" target="_blank" rel="noopener noreferrer">
+              Onchain Ratings
             </LinkButton>
           </LinkRow>
         </RockMenu>
