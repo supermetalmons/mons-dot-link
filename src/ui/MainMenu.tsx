d1dbe8d256309f04d71ce09ab9b8b2435e57510d
ivan grachev <ivan@ivan.lol>
2024-11-03 02:37:37 +0300

    tune ratings button

--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -242,7 +242,7 @@ const MainMenu: React.FC = () => {
         </LinkRow>
         <LinkRow>
           <LinkButton href="https://base.easscan.org/schema/view/0x5c6e798cbb817442fa075e01b65d5d65d3ac35c2b05c1306e8771a1c8a3adb32" target="_blank" rel="noopener noreferrer">
-            Onchain Ratings
+            📈 Onchain Ratings
           </LinkButton>
         </LinkRow>
       </RockMenu>
