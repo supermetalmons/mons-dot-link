1fb2480c216cea6a2eac164fa10e650c5cadd1f3
ivan grachev <ivan@ivan.lol>
2024-11-05 15:35:06 +0300

    generate longer silent audio to avoid looping too frequently

--- src/utils/SoundPlayer.ts
+++ src/utils/SoundPlayer.ts
@@ -10,9 +10,54 @@ export class SoundPlayer {
   constructor() {
     this.audioBufferCache = new Map();
     this.isInitialized = false;
-    this.silentAudio = new Audio("data:audio/mp3;base64,SUQzBAAAAAAAI1RTU0UAAAAPAAADTGF2ZjU4LjIwLjEwMAAAAAAAAAAAAAAA//tUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8AAAACAAABGwBtbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1t//////////////////////////////////////////////////////////////////8AAAAATGF2YzU4LjM1AAAAAAAAAAAAAAAAJAAAAAAAAAAAARsxqNYnAAAAAAAAAAAAAAAAAAAA//sUZAAP8AAAaQAAAAgAAA0gAAABAAABpAAAACAAADSAAAAETEFNRTMuMTAwVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
+
+    const silentAudioUrl = this.createSilentAudioDataUrl(1);
+    this.silentAudio = new Audio(silentAudioUrl);
     this.silentAudio.loop = true;
     this.silentAudio.volume = 0.01;
+  }
+
+  createSilentAudioDataUrl(durationInSeconds: number) {
+    const sampleRate = 8000;
+    const numOfChannels = 1;
+    const bitsPerSample = 8;
+
+    const totalSamples = sampleRate * durationInSeconds;
+    const bytesPerSample = bitsPerSample / 8;
+    const blockAlign = numOfChannels * bytesPerSample;
+    const byteRate = sampleRate * blockAlign;
+    const dataSize = totalSamples * blockAlign;
+
+    const buffer = new ArrayBuffer(44 + dataSize);
+    const view = new DataView(buffer);
+    this.writeString(view, 0, "RIFF");
+    view.setUint32(4, 36 + dataSize, true);
+    this.writeString(view, 8, "WAVE");
+    this.writeString(view, 12, "fmt ");
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+    view.setUint16(22, numOfChannels, true);
+    view.setUint32(24, sampleRate, true);
+    view.setUint32(28, byteRate, true);
+    view.setUint16(32, blockAlign, true);
+    view.setUint16(34, bitsPerSample, true);
+    this.writeString(view, 36, "data");
+    view.setUint32(40, dataSize, true);
+
+    const offset = 44;
+    const silentValue = 128;
+    for (let i = 0; i < totalSamples; i++) {
+      view.setUint8(offset + i, silentValue);
+    }
+
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    return `data:audio/wav;base64,${base64String}`;
+  }
+
+  writeString(view: any, offset: any, string: any) {
+    for (let i = 0; i < string.length; i++) {
+      view.setUint8(offset + i, string.charCodeAt(i));
+    }
   }
 
   public initialize(): void {
